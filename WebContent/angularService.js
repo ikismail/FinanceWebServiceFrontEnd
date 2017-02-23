929d96f1f1a5ea7e5e949354bc1b4a7d809d5029
Mohammed Ismail <ikismail7@gmail.com>
2017-02-23 21:00:31 +0530

    Screenshots

--- WebContent/angularService.js
+++ WebContent/angularService.js
@@ -1,65 +1,67 @@
 /**
- *  Created by ikism on Feb 23, 2017
+ * Created by ikism on Feb 23, 2017
  */
 
-app.factory('angularService',function($http){
+app.factory('angularService', function($http) {
 	console.log('---starting AngularService')
-	
+
+	// Base url of RESTfullservice
 	var BASE_URL = "http://localhost:8098/WebService"
-		
+
 	var angularService = this;
-	
-	angularService.calculateAPR = function(apr){
+
+	// APR object passing to the HTTP Request POST method and responding by
+	// returning an object after calculating APR
+	angularService.calculateAPR = function(apr) {
 		console.log('entering APR calculation')
-		return $http.post(BASE_URL + "/APR/", apr).then(
-				function(response){
-					console.log('calculating APR')
-					console.log('Status : ' + response.status)
-					console.log('Data: ' + response.data)
-					return response.data
-				},function(response){
-					console.log('Error While calculating')
-					return response.data
-				});
+		return $http.post(BASE_URL + "/APR/", apr).then(function(response) {
+			console.log('calculating APR')
+			console.log('Status : ' + response.status)
+			console.log('Data: ' + response.data)
+			return response.data
+		}, function(response) {
+			console.log('Error While calculating')
+			return response.data
+		});
 	};
-	
-	angularService.calculateLeastMP = function(leastmp){
+
+	angularService.calculateLeastMP = function(leastmp) {
 		console.log('entering leastmp calculation')
 		return $http.post(BASE_URL + "/LeastMonthlyPayment/", leastmp).then(
-				function(response){
+				function(response) {
 					console.log('calculating leastmp')
 					console.log('Status : ' + response.status)
 					console.log('Data: ' + response.data)
 					return response.data
-				},function(response){
+				}, function(response) {
 					console.log('Error While calculating')
 					return response.data
 				});
 	};
-	
-	angularService.calculateLoanMP = function(loanmp){
+
+	angularService.calculateLoanMP = function(loanmp) {
 		console.log('entering loanmp calculation')
 		return $http.post(BASE_URL + "/LoanMonthlyPayment/", loanmp).then(
-				function(response){
+				function(response) {
 					console.log('calculating loanmp')
 					console.log('Status : ' + response.status)
 					console.log('Data: ' + response.data)
 					return response.data
-				},function(response){
+				}, function(response) {
 					console.log('Error While calculating')
 					return response.data
 				});
 	};
-	
-	angularService.calculateLoanNumP = function(loanNp){
+
+	angularService.calculateLoanNumP = function(loanNp) {
 		console.log('entering loanNp calculation')
 		return $http.post(BASE_URL + "/LoanNumberPayment/", loanNp).then(
-				function(response){
+				function(response) {
 					console.log('calculating loanNp')
 					console.log('Status : ' + response.status)
 					console.log('Data: ' + response.data)
 					return response.data
-				},function(response){
+				}, function(response) {
 					console.log('Error While calculating')
 					return response.data
 				});
